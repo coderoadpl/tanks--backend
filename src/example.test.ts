ad64a277bb7fc0c8566c4e007505bf571fb51eb1
Mateusz Choma <chomamateusz@gmail.com>
2021-11-27 16:46:10 +0100

    🎉 `express` `ts` `eslint` `jest` added and configured

---
+++ src/example.test.ts
@@ -0,0 +1,7 @@
+describe('dummy', () => {
+  it('should pass', () => {
+    const assertion = true
+    const expectation = true
+    expect(assertion).toBe(expectation)
+  })
+})
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.listen(PORT, () => {
+  console.log('listening on port ' + PORT)
+})
